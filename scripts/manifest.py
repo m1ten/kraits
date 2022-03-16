1656db36e45742201f5bde096895ecad4896b081
miten <git.pub@icloud.com>
2022-03-16 12:58:51 -0400

    28

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -3,7 +3,7 @@
 # Generate a mainfest.yml file for the main directory in a git repository
 
 # Note: This script is intended to be run from the root of the repository
-#       and will be run automatically by the CI server 
+#       and will be run automatically by the CI server
 #	   (see .github/workflows/mainfest.yml)
 
 # Only supports Linux (for now)
@@ -12,37 +12,43 @@ import sys
 import os
 import yaml
 
-yaml.Dumper.ignore_aliases = lambda *args : True
+yaml.Dumper.ignore_aliases = lambda *args: True
 
 # Get specific flag passed to script
+
+
 def get_flag(flag):
-	for i in range(1, len(sys.argv)):
-		if sys.argv[i] == flag:
-			return sys.argv[i+1]
-	return None
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i] == flag:
+            return sys.argv[i+1]
+    return None
+
 
 commit = get_flag('--commit')
 repo = get_flag('--repo')
-		
+
 # Get list of dirs in a subdir that changed in the last commit
+
+
 def get_changed_dirs(subdir):
-	changed_dirs = []
-	for line in os.popen('git show --name-only --pretty=format:"" --diff-filter=ACMRTUXB ' + commit).readlines():
-		if line.startswith(subdir):
+    changed_dirs = []
+    for line in os.popen('git show --name-only --pretty=format:"" --diff-filter=ACMRTUXB ' + commit).readlines():
+        if line.startswith(subdir):
 
-			# Check if line is a file or a directory
-			if os.path.isfile(line.strip()):
-				changed_dirs.append(os.path.dirname(line))
-			
-	# Remove duplicates and return
+            # Check if line is a file or a directory
+            if os.path.isfile(line.strip()):
+                changed_dirs.append(os.path.dirname(line))
 
-	return list(set(changed_dirs))
+    # Remove duplicates and return
+
+    return list(set(changed_dirs))
+
 
 dirs = get_changed_dirs('manifest')
 
 # Read manifest.yml
 with open('manifest.yml', 'r') as m:
-	mf_file = m.read()
+    mf_file = m.read()
 
 # Get yaml data from mf_file
 mf_yaml = yaml.load(mf_file, Loader=yaml.FullLoader)
@@ -51,17 +57,17 @@ print(mf_yaml)
 
 # Check if mf_yaml is a empty dict
 if not mf_yaml:
-	print('manifest.yml is empty.')
-	mf_yaml = {}
+    print('manifest.yml is empty.')
+    mf_yaml = {}
 
-# Get name of repo 
+# Get name of repo
 mf_yaml['name'] = 'neopkgs'
 mf_yaml['latest_commit'] = commit
 mf_yaml['packages'] = {}
 
 # Check if dirs is empty
 if len(dirs) == 0:
-	print('No directories changed in manifest directory.')
+    print('No directories changed in manifest directory.')
 
 # Remove empty dirs
 dirs = [d for d in dirs if d != '']
@@ -70,53 +76,55 @@ print(dirs)
 
 # Get list of files in each folder
 for dir in dirs:
-	print(dir)
-	mf = {}
-	mf['commit'] = commit
-	mf['contents'] = []
-	
-	files = os.listdir(dir)
-	for file in files:
-		# Get file name with extension
-		if (dir + "/" + file).endswith(".yml"):
+    print(dir)
+    mf = {}
+    mf['commit'] = commit
+    mf['contents'] = []
 
-			# Read from f.yml in dir
-			with open(f'{dir + "/" + file}', 'r') as f:
-				f_file = f.read()
-				f_yaml = yaml.load(f_file, Loader=yaml.FullLoader)
+    files = os.listdir(dir)
+    for file in files:
+        # Get file name with extension
+        if (dir + "/" + file).endswith(".yml"):
 
-				try:
-					ver = f_yaml['ver']
-				except KeyError: 
-					print('Invalid yaml file: ' + file)
+            # Read from f.yml in dir
+            with open(f'{dir + "/" + file}', 'r') as f:
+                f_file = f.read()
+                f_yaml = yaml.load(f_file, Loader=yaml.FullLoader)
 
-		f_dict = {}
-		f_dict['name'] = file
-		f_dict['path'] = dir + "/" + file
-		
-		# Get sha256sum of file in dir
-		os.system(f"sha256sum {f_dict['path']} > {f_dict['path']}.sha256sum")
-		with open(f"{f_dict['path']}.sha256sum", 'r') as f:
-			f_dict['sha256'] = f.read().split()[0]
-		os.remove(f"{f_dict['path']}.sha256sum")
+                try:
+                    ver = f_yaml['ver']
+                except KeyError:
+                    print('Invalid yaml file: ' + file)
 
-		# Get url of file in dir
-		f_dict['url'] = f"https://raw.githubusercontent.com/{repo}/{commit}/{f_dict['path']}"
+        f_dict = {}
+        f_dict['name'] = file
+        f_dict['path'] = dir + "/" + file
 
-		mf['contents'].append(f_dict)
+        # Get sha256sum of file in dir
+        os.system(f"sha256sum {f_dict['path']} > {f_dict['path']}.sha256sum")
+        with open(f"{f_dict['path']}.sha256sum", 'r') as f:
+            f_dict['sha256'] = f.read().split()[0]
+        os.remove(f"{f_dict['path']}.sha256sum")
 
-	pkg = dir.replace('manifests/', '')
+        # Get url of file in dir
+        f_dict['url'] = f"https://raw.githubusercontent.com/{repo}/{commit}/{f_dict['path']}"
 
-	mf_yaml['packages'][pkg] = {}
-	mf_yaml['packages'][pkg][ver] = []
+        mf['contents'].append(f_dict)
 
-	if mf in mf_yaml['packages'][pkg][ver]:
-		print(f"Modified {pkg} - {ver} already exists in manifest.yml")
-	else:
-		mf_yaml['packages'][pkg][ver].append(mf)
+    pkg = dir.replace('manifests/', '')
 
-	print(mf)
+    mf_yaml['packages'][pkg] = {} if mf_yaml['packages'].get(
+        pkg) is None else mf_yaml['packages'][pkg]
+    mf_yaml['packages'][pkg][ver] = [] if mf_yaml['packages'][pkg].get(
+        ver) is None else mf_yaml['packages'][pkg][ver]
+
+    if mf in mf_yaml['packages'][pkg][ver]:
+        print(f"Modified {pkg} - {ver} already exists in manifest.yml")
+    else:
+        mf_yaml['packages'][pkg][ver].append(mf)
+
+    print(mf)
 
 # Write to manifest.yml
 with open('manifest.yml', 'w') as m:
-	m.write(yaml.dump(mf_yaml, default_flow_style=False, sort_keys=False))
+    m.write(yaml.dump(mf_yaml, default_flow_style=False, sort_keys=False))
